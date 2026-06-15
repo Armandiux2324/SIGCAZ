dc11e293096dc28171f1d9b433db86fc8f0623be
Armandiux2324 <armandocandelasalvarado@gmail.com>
2026-06-14 20:12:50 -0600

    implementación de middleware

--- SIGCAZ-Backend/routes/api.php
+++ SIGCAZ-Backend/routes/api.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Api\UserController;
 
 Route::prefix('v1')->group(function () {
-    // Rutas de autenticación y usuarios
+    // Ruta de autenticación
     Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-    Route::post('/users', [UserController::class, 'store']);
+
+    Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+        // Rutas de usuarios
+        Route::post('/users', [UserController::class, 'store']);
+    });
 
     Route::middleware(['auth:sanctum', 'verified'])->group(function () {
         // Route::get('/me', [UserController::class, 'me']);
