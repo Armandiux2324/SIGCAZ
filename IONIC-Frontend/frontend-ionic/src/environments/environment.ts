d052efa8af6a6b6497f9bdaf37b2206e8fccc6b8
Armandiux2324 <armandocandelasalvarado@gmail.com>
2026-08-12 16:49:27 -0600

    cambios en app

--- IONIC-Frontend/frontend-ionic/src/environments/environment.ts
+++ IONIC-Frontend/frontend-ionic/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'https://labs-civic-shareholders-never.trycloudflare.com/api/v1'
+  base_url: 'http://localhost:8000/api/v1'
 };
 
 /*
